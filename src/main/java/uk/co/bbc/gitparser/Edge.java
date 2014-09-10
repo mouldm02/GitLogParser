cb6d165fb6b4b2ff8d260cd5c0bd9949a6e46df9
Matt Mould <matthew.mould@bbc.co.uk>
2014-09-10 10:05:37 +0100

    Add edge

---
+++ src/main/java/uk/co/bbc/gitparser/Edge.java
@@ -0,0 +1,10 @@
+package uk.co.bbc.gitparser;
+
+public class Edge {
+	String source;
+	String target;
+	public Edge(String source, String target) {
+		this.source = source;
+		this.target = target;
+	}
+}
